6dbd30e970d847f5aba749bfd6dabdb27abc68f3
Ryan Chenkie <chenkie@users.noreply.github.com>
2016-11-10 02:28:46 -0600

    Update AuthService.js

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -57,17 +57,6 @@ export default class AuthService extends EventEmitter {
     localStorage.removeItem('token')
   }
 
-  _checkStatus(response) {
-    // raises an error in case response status is not a success
-    if (response.status >= 200 && response.status < 300) {
-      return response
-    } else {
-      var error = new Error(response.statusText)
-      error.response = response
-      return error
-    }
-  }
-
   fetch(url, options) {
     // performs api calls sending the required authentication headers
     const headers = {
